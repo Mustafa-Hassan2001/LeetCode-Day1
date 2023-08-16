58a315949d820dd474a5013954f65505f1aa7988
Mustafa Hassan <98483655+Mustafa-Hassan2001@users.noreply.github.com>
2023-08-17 02:11:28 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+//	int n ,sum=0;
+//	cin>>n;
+//	
+//	for(int i=1;i<=n;i++){
+//		sum = sum + i;
+//	}
+//	cout<<"The Result is :"<<sum;
+
+
+//fib series
+//	int num1=0, num2=1;
+//	
+//	for(int i=0; i<n;i++){
+//		int nextnum = num1 + num2;
+//		cout<<nextnum<<" ";
+//		num1=num2;
+//		num2=nextnum;
+//	} 
+//	int check = 0;
+//	for(int i=2; i<=(n-1); i++){
+//		if(n%i==0){
+//			check = 1;	
+//			break;
+//		}
+//		else{
+//			check = 0;
+//		}
+//	}
+//	if(check==1){
+//		cout<<"Not Prime";
+//	}
+//	if(check == 0){
+//		cout<<"Prime";
+//	}
+
+//for(int i=1; i<=n; i++){
+//	cout<<"MAJU"<<endl;
+//	cout<<"University"<<endl;
+//	continue;
+//	cout<<"hghgyv";
+//}
+//Left shift << multiplaction lf may padding zero hoti hai (paddinig add new number)
+//Spical case: if i use lf in big +ve number so it become -ve number
+//cout<<(5<<2) <<endl;//5*2->10*2->20
+//cout<<(19>>1)<<endl;//19/2->9
+//cout<<(17>>2)<<endl;//17/2->8/2->4
+////Right shift << ( num / 2*times you do) Rf may padding compiler depended hoti hai (paddinig add new number)
+//cout<<(19<<1)<<endl;//19*2->38
+//cout<<(21<<2)<<endl;//21*2->42*2->84
+
+//Leetcode Day1 P1
+//int n;
+//cin>>n;
+//int sum=0, prd=1, n1=0, res=0;
+//while(n!=0){
+//	n1 = n%10;
+//	n = n/10;
+//	prd =prd * n1;
+//	sum = sum + n1;
+//}
+//res = prd - sum;
+//cout<<res;
+
+
+//Leetcode Day1 P2
+//int n;
+//cin>>n;
+//int sum=0;
+//while(n!=0){
+//	if(n&1){
+//		sum++;
+//	}
+//	n = n>>1;
+//}
+//cout<<sum;
+
+ return 0;
+}
